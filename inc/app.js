b1369bd32d4490a02513e9d305ef4b4af9512d21
drihyd <techsupport@deepredink.com>
2020-10-20 14:56:56 +0530

    updating footer

--- inc/app.js
+++ inc/app.js
@@ -84,6 +84,56 @@ const Footer = ({
 		footerNav
 	}) =>
 	`
+<section class="footer-section" style="background: #24252D">
+    <div class="container">
+      <div class="row">
+        <div class="footer-item">
+          <div class="footer-wrapper">
+            <h4>THE PROJECT</h4>
+            <a href="">About The Project</a>
+            <a href="">The Neighbourhood</a>
+            <a href="">Ameenpur Advantage</a>
+          </div>
+        </div>
+        <div class="footer-item">
+          <div class="footer-wrapper">
+            <h4>FLOOR PLANS</h4>
+            <a href="">2 BHK Floor plans</a>
+            <a href="">3 BHK Floor plans</a>
+          </div>
+        </div>
+        <div class="footer-item">
+          <div class="footer-wrapper">
+            <h4>OTHER LINKS</h4>
+            <a href="">Blogs</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Terms & Conditions</a>
+          </div>
+        </div>
+        <div class="footer-item">
+          <div class="footer-wrapper">
+            <h4>URBANRISE</h4>
+            <a href="">About</a>
+            <a href="">Hyderabad Projects</a>
+            <a href="">Chennai Projects</a>
+          </div>
+        </div>
+        <div class="footer-item">
+          <div class="footer-wrapper">
+            <h4>CONTACT</h4>
+            <p>Write to us:</p>
+            <a href="">connect@springisintheair.in</a>
+            <p>Call us:</p>
+            <a href="tel:+91-(902)-214-5512">+91-(902)-214-5512</a>
+            <p>Visit us:</p>
+            <a href="">Ameenpur, Hyderabad-45</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
  <footer class="call-footer-fixed" style="background: #1D1E24">
 
     <div class="container">
